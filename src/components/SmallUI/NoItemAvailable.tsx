cf1a31dc85373ea58291119d08b6b7981d49d9eb
shahriar hasan <shahriar.hasan.1523@gmail.com>
2024-03-01 02:16:16 +0600

    basic build success preview upload

--- src/components/SmallUI/NoItemAvailable.tsx
+++ src/components/SmallUI/NoItemAvailable.tsx
@@ -3,7 +3,7 @@ import { MdOutlineContentPasteSearch } from "react-icons/md";
 
 const NoItemAvailable = ({ text }: { text: string }) => {
   return (
-    <div className="flex justify-center items-center my-2 border max-w-xl w-full">
+    <div className="flex justify-center items-center my-2 border max-w-xl w-full mx-auto py-10">
       <div className="flex flex-col items-center p -10 gap-10 text-center">
         <MdOutlineContentPasteSearch className="text-4xl text-dimmed" />
         <h1 className="text-2xl font-medium text-dimmed tracking-widest">{text}</h1>
